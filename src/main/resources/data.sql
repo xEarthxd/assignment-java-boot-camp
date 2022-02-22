e7791a6e24832cbf2384c14e9c80d47fedff0cd3
Distat Permpoonwiwat <distat@doctorraksa.com>
2022-02-22 16:15:45 +0700

    chore: Setup H2 database schemas and populate user/product data #2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,46 @@
+INSERT INTO user (id, first_name, last_name, address, created_at, modified_at) VALUES
+    (1, 'John', 'Mayer', '142/34-5  Bang Phueng, Pra Pradaeng, Samutprakarn, Thailand 10130', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, 'Peter', 'Parker', '5 Soi Ekamai Sukhumvit 63 Road Klongton, Bangkok, Thailand 10110', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3, 'Bruce', 'Wayne', '255/220 Soi Latphrao 48 Latphrao Road Samsennok, Huaikhwang, Bangkok 10310', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO product (id, name, price, description, created_at, modified_at) VALUES
+    (1, 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops', 109.95, 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (2, 'Mens Casual Premium Slim Fit T-Shirts ', 22.3, 'Slim-fitting style, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (3, 'Mens Cotton Jacket', 55.99, 'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (4, 'Mens Casual Slim Fit', 15.99, 'The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (5, 'John Hardy Women"s Legends Naga Gold & Silver Chain Bracelet', 695, 'From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean"s pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (6, 'Solid Gold Petite Micropave ', 168, 'Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (7, 'White Gold Plated Princess', 9.99, 'Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine"s Day...', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (8, 'Pierced Owl Rose Gold Plated Stainless Steel Double', 10.99, 'Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (9, 'WD 2TB Elements Portable External Hard Drive - USB 3.0 ', 64, 'USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (10, 'SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s', 109, 'Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Performance may vary depending upon drive capacity, host device, OS and application.)', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (11, 'Silicon Power 256GB SSD SATA III 2.5', 109, '3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (12, 'WD 4TB Gaming Drive Works Portable External Hard Drive', 114, 'Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer"s limited warranty', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (13, 'Acer SB220Q 21.5 inches Full HD (1920 x 1080) IPS', 599, '21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (14, 'Samsung 49-Inch 144Hz Curved Gaming Monitor', 999.99, '49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (15, 'BIYLACLESEN Women"s 3-in-1 Snowboard Jacket Winter Coats', 56.99, '100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (16, 'Women"s Removable Hooded Biker Jacket', 29.95, '100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (17, 'Rain Jacket Women Windbreaker Climbing Raincoats', 39.99, 'Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (18, 'MBJ Women"s Solid Short Sleeve Boat Neck V', 9.85, '95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (19, 'Opna Women"s Short Sleeve Moisture', 7.95, '100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+
+    (20, 'DANVOUY Womens T Shirt Casual Cotton Short', 12.99, '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+    id  INTEGER PRIMARY KEY,
+    first_name  VARCHAR(64) NOT NULL,
+    last_name   VARCHAR(64) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP,
+    modified_at TIMESTAMP
+);
+
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product (
+    id  INTEGER PRIMARY KEY,
+    name  VARCHAR(64) NOT NULL,
+    price  FLOAT,
+    description VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP,
+    modified_at TIMESTAMP
+);
+
+DROP TABLE IF EXISTS cart;
+
+CREATE TABLE cart (
+    id  INTEGER PRIMARY KEY,
+    user_id  INTEGER,
+    created_at  TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+DROP TABLE IF EXISTS cart_item;
+
+CREATE TABLE cart_item (
+    id  INTEGER PRIMARY KEY,
+    cart_id  INTEGER,
+    product_id INTEGER,
+    quantity  INTEGER,
+    created_at  TIMESTAMP,
+    modified_at  TIMESTAMP,
+    FOREIGN KEY (cart_id) REFERENCES cart(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order` (
+    id  INTEGER PRIMARY KEY,
+    user_id  INTEGER,
+    status  INTEGER, -- Will be replaced with enum when implementing order flow
+    created_at  TIMESTAMP,
+    modified_at  TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+DROP TABLE IF EXISTS order_item;
+
+CREATE TABLE order_item (
+    id  INTEGER PRIMARY KEY,
+    order_id INTEGER,
+    product_id  INTEGER,
+    quantity  INTEGER,
+    created_at  TIMESTAMP,
+    modified_at  TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES `order`(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
