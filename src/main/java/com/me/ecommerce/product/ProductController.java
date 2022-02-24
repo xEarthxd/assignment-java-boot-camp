d142746dfade1dcc3aff9afdb59f7f19dc3c9ce5
Distat Permpoonwiwat <distat@doctorraksa.com>
2022-02-24 12:05:42 +0700

    feat(Product): Create api endpoint that will search for products with specific keyword #4

--- src/main/java/com/me/ecommerce/product/ProductController.java
+++ src/main/java/com/me/ecommerce/product/ProductController.java
@@ -2,6 +2,7 @@ package com.me.ecommerce.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,8 +13,16 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepo;
 
-    @GetMapping("/api/products")
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value="/api/products")
     public List<Product> getAllProduct() {
         return productRepo.findAll();
+    }
+
+    @GetMapping(value="/api/products", params="keyword")
+    public List<Product> getAllByKeyword(@RequestParam(name="keyword") String keyword) {
+        return productService.getAllByKeyword(keyword);
     }
 }
--- src/test/java/com/me/ecommerce/product/ProductControllerTest.java
+++ src/test/java/com/me/ecommerce/product/ProductControllerTest.java
@@ -3,9 +3,15 @@ package com.me.ecommerce.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductControllerTest {
@@ -13,19 +19,39 @@ class ProductControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @MockBean
+    private ProductService productService;
+
     @Test
     void shouldReturnTwentyElementsWhenRequestingForAllProducts() {
         // Arrange
-        // Since we are using data.sql to pre-insert the data
-        // this test will also use the same datasource
-        // (which should not happen because test needs to be isolated)
-        // we will improve this when implementing Postgres for main app
+        /*
+            Since we are using data.sql to pre-insert the data
+            this test will also use the same datasource
+            (which should not happen because test needs to be isolated)
+            we will improve this when implementing Postgres for main app
+        */
 
         // Act
         Product[] result = testRestTemplate.getForObject("/api/products", Product[].class);
 
         // Assert
         assertEquals(20, result.length);
+    }
 
+    @Test
+    void shouldReturnProductsContainingKeywordRequestedWithKeywordQueryString() {
+        // Arrange
+        List<Product> stubProducts = new ArrayList<>();
+        stubProducts.add(new Product(100, "Test1", 10.0f, "Contain keyword", new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis())));
+        stubProducts.add(new Product(101, "test2", 20.0f, "Contain keyword", new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis())));
+        stubProducts.add(new Product(102, "tEst3", 40.0f, "Contain keyword", new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis())));
+        when(productService.getAllByKeyword("test")).thenReturn(stubProducts);
+
+        // Act
+        Product[] result = testRestTemplate.getForObject("/api/products?keyword=test", Product[].class);
+
+        // Assert
+        assertEquals(3, result.length);
     }
 }
