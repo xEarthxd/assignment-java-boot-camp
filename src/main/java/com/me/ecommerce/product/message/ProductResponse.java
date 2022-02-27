7ebd0797089a96e2abf7e3e2e6c2c1fde7acd94a
Distat Permpoonwiwat <distat@doctorraksa.com>
2022-02-27 22:09:59 +0700

    refactor(Product): Created ProductResponse model to replace Product as return type

---
+++ src/main/java/com/me/ecommerce/product/message/ProductResponse.java
@@ -0,0 +1,50 @@
+package com.me.ecommerce.product.message;
+
+public class ProductResponse {
+    private int id;
+    private String name;
+    private float price;
+    private String description;
+
+    public ProductResponse() {
+    }
+
+    public ProductResponse(int id, String name, float price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
