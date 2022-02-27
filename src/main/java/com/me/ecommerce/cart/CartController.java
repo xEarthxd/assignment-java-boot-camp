042151de6b379d8139d53aa312c4fd6bcf6e7020
Distat Permpoonwiwat <distat@doctorraksa.com>
2022-02-27 19:26:33 +0700

    feat(Cart): Created endpoint to view user's cart #8

--- src/main/java/com/me/ecommerce/cart/CartController.java
+++ src/main/java/com/me/ecommerce/cart/CartController.java
@@ -1,11 +1,12 @@
 package com.me.ecommerce.cart;
 
 import com.me.ecommerce.cart.message.AddItemRequest;
+import com.me.ecommerce.cart.message.ViewCartResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CartController {
@@ -17,9 +18,13 @@ public class CartController {
             value = "/api/cart/add-item",
             consumes = {MediaType.APPLICATION_JSON_VALUE}
     )
-
     public String addItemToCart(@RequestBody AddItemRequest req) {
         return cartService.addItemToCart(req.getUserId(), req.getProductId(), req.getQuantity());
+    }
+
+    @GetMapping(value = "/api/cart/view-items")
+    public ResponseEntity<ViewCartResponse> viewCart(@RequestHeader("user-id") int userId) {
+        return new ResponseEntity<ViewCartResponse>(cartService.viewCart(userId), HttpStatus.OK);
     }
 
 }
--- src/test/java/com/me/ecommerce/cart/CartControllerTest.java
+++ src/test/java/com/me/ecommerce/cart/CartControllerTest.java
@@ -1,13 +1,23 @@
 package com.me.ecommerce.cart;
 
 import com.me.ecommerce.cart.message.AddItemRequest;
+import com.me.ecommerce.cart.message.ItemInfo;
+import com.me.ecommerce.cart.message.ViewCartResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,7 +30,8 @@ class CartControllerTest {
     private CartService cartService;
 
     @Test
-    void addItemToCart() {
+    @DisplayName("Should return success message when add item to cart successfully")
+    void addItemToCartTest() {
         // Arrange
         AddItemRequest reqBody = new AddItemRequest(100, 1, 10);
         String stubServiceReturn = String.format("Successfully added %d quantity of productId: %d for user: %d", reqBody.getQuantity(), reqBody.getProductId(), reqBody.getUserId());
@@ -31,5 +42,30 @@ class CartControllerTest {
 
         // Assert
         assertEquals("Successfully added 10 quantity of productId: 1 for user: 100", response);
+    }
+
+    @Test
+    @DisplayName("Should return 2 mock products when viewing cart item for mock user")
+    void viewCartItemTest() {
+        // Arrange
+        ItemInfo mockInfo1 = new ItemInfo(100, "MockProduct1", 100.0f, "Mock Product For Test1");
+        ItemInfo mockInfo2 = new ItemInfo(101, "MockProduct2", 20.0f, "Mock Product For Test2");
+        List<ItemInfo> mockItemsInfo = new ArrayList<>();
+        mockItemsInfo.add(mockInfo1);
+        mockItemsInfo.add(mockInfo2);
+
+        ViewCartResponse stubServiceReturn = new ViewCartResponse(100, 1, 2, mockItemsInfo);
+        when(cartService.viewCart(100)).thenReturn(stubServiceReturn);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("user-id", "100");
+
+
+        // Act
+        ResponseEntity<ViewCartResponse> response = testRestTemplate.exchange("/api/cart/view-items", HttpMethod.GET, new HttpEntity<>(headers), ViewCartResponse.class);
+        ViewCartResponse cartResponse = response.getBody();
+
+        // Assert
+        assertEquals(2, cartResponse.getCount());
     }
 }
