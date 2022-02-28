f13477a03c6d5e587dc770cecb9ef28b03062817
Distat Permpoonwiwat <distat@doctorraksa.com>
2022-02-28 11:35:37 +0700

    feat(Cart): Created Order and OrderItem entity use for Cart Checkout #10

---
+++ src/main/java/com/me/ecommerce/order/Order.java
@@ -0,0 +1,83 @@
+package com.me.ecommerce.order;
+
+import com.me.ecommerce.cart.model.CartItem;
+import com.me.ecommerce.user.model.User;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name="order_t")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "modified_at")
+    private Timestamp modifiedAt;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    public Order() {
+    }
+
+    public Order(User user, String status, Timestamp createdAt, Timestamp modifiedAt) {
+        this.user = user;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Timestamp modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+}
---
+++ src/main/java/com/me/ecommerce/order/OrderItem.java
@@ -0,0 +1,90 @@
+package com.me.ecommerce.order;
+
+import com.me.ecommerce.product.model.Product;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+
+    @Column(name="quantity")
+    private int quantity;
+
+    @Column(name="created_at")
+    private Timestamp createdAt;
+
+    @Column(name="modified_at")
+    private Timestamp modifiedAt;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, int quantity, Timestamp createdAt, Timestamp modifiedAt) {
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Timestamp modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+}
